625fb83feea9cf0fe55e61aedbdc957f3e55ed5d
Joao Azevedo <devjoaoazevedo@gmail.com>
2021-10-03 19:31:43 -0300

    test: ensure RemoteAuthentication throws unexpectedError on default cases

--- src/data/mock/mock-http-client.ts
+++ src/data/mock/mock-http-client.ts
@@ -14,7 +14,7 @@ export class HttpPostClientSpy implements HttpPostClient {
   public body?: object;
 
   public response: HttpResponse = {
-    statusCode: HttpStatusCode.noContent,
+    statusCode: HttpStatusCode.ok,
   };
 
   async post(params: HttpPostParams): Promise<HttpResponse> {
--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -6,6 +6,7 @@ import { HttpStatusCode } from "@/data/protocols/http/http-response";
 
 import { mockAuthentication } from "@/domain/mock/mock-authentication";
 import { InvalidCredentialsError } from "@/domain/errors/Invalid-credentials-error";
+import { UnexpectedError } from "@/domain/errors/unexpected-error";
 
 import { RemoteAuthentication } from "./remote-authentication";
 
@@ -55,5 +56,41 @@ describe("RemoteAuthentication", () => {
     const promise = sut.auth(mockAuthentication());
 
     await expect(promise).rejects.toThrow(new InvalidCredentialsError());
+  });
+
+  test("Should throw UnexpectedError if HttpPostClient returns status 400", async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.badRequest,
+    };
+
+    const promise = sut.auth(mockAuthentication());
+
+    await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
+
+  test("Should throw UnexpectedError if HttpPostClient returns status 500", async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.serverError,
+    };
+
+    const promise = sut.auth(mockAuthentication());
+
+    await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
+
+  test("Should throw UnexpectedError if HttpPostClient returns status 404", async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.notFound,
+    };
+
+    const promise = sut.auth(mockAuthentication());
+
+    await expect(promise).rejects.toThrow(new UnexpectedError());
   });
 });
