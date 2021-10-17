206d35a8f26f36cf7988a8bbf7d718545e5c27e9
Joao Azevedo <devjoaoazevedo@gmail.com>
2021-10-17 10:17:16 -0300

    fix: add jest mock to files and styles

--- jest.config.ts
+++ jest.config.ts
@@ -10,9 +10,11 @@ export default {
   testEnvironment: "jsdom",
   transform: {
     ".+\\.(ts|tsx)$": "ts-jest",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileTransformer.js",
   },
   moduleNameMapper: {
     "@/(.*)": "<rootDir>/src/$1",
-    "\\.scss$": "identity-obj-proxy",
+    "\\.(css|scss)$": "identity-obj-proxy",
   },
 };
