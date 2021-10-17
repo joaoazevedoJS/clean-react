d0bde7ff42d208703d16fca2d40ecc11610d2807
Joao Azevedo <devjoaoazevedo@gmail.com>
2021-10-17 16:01:08 -0300

    test: ensure form status render spinner and message error when has value

---
+++ src/presentation/components/form-status/form-status.spec.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { mocked } from "ts-jest/utils";
+import { render, screen } from "@testing-library/react";
+
+import {
+  useFormContext,
+  FormContextPropsMocked,
+} from "@/presentation/contexts/form/form-context";
+
+import FormStatus from "./form-status";
+
+jest.mock("@/presentation/contexts/form/form-context");
+
+function useFormContextMocked({
+  isLoading,
+  errorMessage,
+}: FormContextPropsMocked) {
+  const contextMocked = mocked(useFormContext);
+
+  contextMocked.mockReturnValueOnce({
+    isLoading: !!isLoading,
+    errorMessage: errorMessage || "",
+  });
+}
+
+describe("Form status component", () => {
+  it("should not be able to render spinner load when not has loading", () => {
+    useFormContextMocked({});
+
+    render(<FormStatus />);
+
+    const spinner = screen.queryByRole("status", {
+      name: /spinner loading/i,
+    });
+
+    expect(spinner).not.toBeInTheDocument();
+  });
+
+  it("should be able to render spinner load when has loading", () => {
+    useFormContextMocked({
+      isLoading: true,
+    });
+
+    render(<FormStatus />);
+
+    const spinner = screen.getByRole("status", {
+      name: /spinner loading/i,
+    });
+
+    expect(spinner).toBeInTheDocument();
+  });
+
+  it("should not be able to render error message when has empty message", () => {
+    useFormContextMocked({});
+
+    render(<FormStatus />);
+
+    const spinner = screen.queryByRole("alert", {
+      name: /error message/i,
+    });
+
+    expect(spinner).not.toBeInTheDocument();
+  });
+
+  it("should be able to render error message when has message", () => {
+    useFormContextMocked({
+      errorMessage: "error message mocked",
+    });
+
+    render(<FormStatus />);
+
+    const spinner = screen.getByRole("alert", {
+      name: /error message/i,
+    });
+
+    expect(spinner).toBeInTheDocument();
+  });
+});
--- src/presentation/contexts/form/form-context.ts
+++ src/presentation/contexts/form/form-context.ts
@@ -1,13 +1,19 @@
 import { createContext, useContext } from "react";
 
-export type StateProps = {
+export type FormContextProps = {
   isLoading: boolean;
   errorMessage: string;
 };
 
-export const FormContext = createContext<StateProps>({} as StateProps);
+export type FormContextPropsMocked = {
+  [key in keyof FormContextProps]?: FormContextProps[key];
+};
 
-export function useFormContext(): StateProps {
+export const FormContext = createContext<FormContextProps>(
+  {} as FormContextProps
+);
+
+export function useFormContext(): FormContextProps {
   const context = useContext(FormContext);
 
   return context;
