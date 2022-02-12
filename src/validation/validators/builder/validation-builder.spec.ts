e0fbabd95ebf0ab1c918b39dfd20e41292bd77b6
Joao Azevedo <devjoaoazevedo@gmail.com>
2022-02-12 10:25:25 -0300

    test: ensure validationBuilder has a email  helper

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -5,6 +5,7 @@ import faker from "@faker-js/faker";
 import {
   RequiredFieldValidation,
   ValidationBuilder,
+  EmailValidation,
 } from "@/validation/validators";
 
 describe("ValidationBuilder", () => {
@@ -14,5 +15,13 @@ describe("ValidationBuilder", () => {
     const validations = ValidationBuilder.field(field).required().build();
 
     expect(validations).toEqual([new RequiredFieldValidation(field)]);
+  });
+
+  test("Should return EmailValidation", () => {
+    const field = faker.database.column();
+
+    const validations = ValidationBuilder.field(field).email().build();
+
+    expect(validations).toEqual([new EmailValidation(field)]);
   });
 });
